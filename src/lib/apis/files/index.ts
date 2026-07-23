fba38c222d6d046874891af4266d70165bda6aea
ahmedmarzougui-enclaive <ahmed.marzougui@enclaive.io>
2026-07-23 15:12:51 +0100

    fix: add getFileCount to files/index.ts missing after upstream merge

--- src/lib/apis/files/index.ts
+++ src/lib/apis/files/index.ts
@@ -369,3 +369,31 @@ export const deleteAllFiles = async (token: string) => {
 
 	return res;
 };
+
+export const getFileCount = async (token: string = '') => {
+	let error = null;
+
+	const res = await fetch(`${WEBUI_API_BASE_URL}/files/count`, {
+		method: 'GET',
+		headers: {
+			Accept: 'application/json',
+			'Content-Type': 'application/json',
+			authorization: `Bearer ${token}`
+		}
+	})
+		.then(async (res) => {
+			if (!res.ok) throw await res.json();
+			return res.json();
+		})
+		.catch((err) => {
+			error = err;
+			console.error(err);
+			return null;
+		});
+
+	if (error) {
+		throw error;
+	}
+
+	return res;
+};
